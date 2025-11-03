9363b09e38eccc78cde67f01c9a71443931097d2
Maksym-Sirenko <toyotasan@gmail.com>
2025-11-03 16:58:21 +0200

    need-more-little-fixesgit add .!

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,4 @@
-// import { useRouter } from 'next/navigation';
-// import { useEffect } from 'react';
 import css from './not-found.module.css';
-// import type { Metadata } from 'next';
 import { VERSEL_URL, IMAGE_URL } from '@/lib/constants';
 
 export const metadata = {
@@ -23,20 +20,12 @@ export const metadata = {
 };
 
 const NotFound = () => {
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => router.push('/'), 3000);
-  //   return () => clearTimeout(timer);
-  // }, [router]);
-
   return (
     <div className={css.container}>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
-      {/* <p className={css.redirect}>Redirecting to homepage in 3 seconds...</p> */}
     </div>
   );
 };
